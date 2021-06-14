8bbd172c961a729fae50bf63f0cf5cbfa78e6db4
flavio <flaviozanda@gmail.com>
2021-06-14 22:14:07 +0200

    Minor changes in styling for login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -6,7 +6,7 @@ import {persistToken, persistUser} from "../common/persistance/Persistance";
 import {useTranslation} from "../common/i18n/UseTranslation";
 import './Login.css';
 
-export const Login: React.FC<{ }> = () => {
+export const Login: React.FC<{}> = () => {
 
     const {t} = useTranslation();
     const [username, setUsername] = useState("");
@@ -14,70 +14,70 @@ export const Login: React.FC<{ }> = () => {
     const history = useHistory();
 
     return (
-        <body>
+        <div className={'body'}>
             <div className="leftBox">
-                </div>
-        <section>
-            <div className="imgBox">
-                <img src="bg.jpg"/>
             </div>
-            <div className="contentBox">
-                <div className="formBox">
-                    <h2>Login</h2>
+            <section>
+                <div className="imgBox">
+                    <img src="bg.jpg"/>
+                </div>
+                <div className="contentBox">
+                    <div className="formBox">
+                        <h2>Login</h2>
                         <form onSubmit={
-                        (e) => {
-                        e.preventDefault();
-                        login(username, password)
-                            .then((o) => {
-                                persistToken(o.token);
-                                persistUser(o.id);
+                            (e) => {
+                                e.preventDefault();
+                                login(username, password)
+                                    .then((o) => {
+                                        persistToken(o.token);
+                                        persistUser(o.id);
 
-                                if (o.userType === "admin") {
-                                    history.push("/a/dashboard");
-                                } else if (o.userType === "manager") {
-                                    history.push("/m/dashboard");
-                                } else if (o.userType === "owner") {
-                                    history.push("/o/dashboard");
-                                }
-                            })
-                            .catch((e) => {
-                                console.log(e);
-                                // HANDLE FAILURE
-                            })
-                        console.log("submitting");
-                    }
-                }>
-                <span>E-mail</span>
-                <div className="inputBox">
-                    <input
-                        id="email"
-                        type={'string'}
-                        onChange={(e) => setUsername(e.target.value)}
-                        required
-                    />
+                                        if (o.userType === "admin") {
+                                            history.push("/a/dashboard");
+                                        } else if (o.userType === "manager") {
+                                            history.push("/m/dashboard");
+                                        } else if (o.userType === "owner") {
+                                            history.push("/o/dashboard");
+                                        }
+                                    })
+                                    .catch((e) => {
+                                        console.log(e);
+                                        // HANDLE FAILURE
+                                    })
+                                console.log("submitting");
+                            }
+                        }>
+                            <span className={'formLabel'}>E-mail</span>
+                            <div className="inputBox">
+                                <input
+                                    id="email"
+                                    type={'string'}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <span className={'formLabel'}>Password</span>
+                            <div className="inputBox">
+                                <input
+                                    id="password"
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    type={'password'}
+                                    required
+                                />
+                            </div>
+                            <div className="rememberMe">
+                                <label><input type="checkbox" name="my"/> Remember me</label>
+                            </div>
+                            <div className="loginBox">
+                                <Button type={'submit'}>
+                                    {t.common.login.loginButton}
+                                </Button>
+                            </div>
+                        </form>
                     </div>
-                    <span>Password</span>
-                    <div className="inputBox">
-                    <input
-                        id="password"
-                        onChange={(e) => setPassword(e.target.value)}
-                        type={'password'}
-                        required
-                    />
-                    </div>
-                    <div className="rememberMe">
-                        <label><input type="checkbox" name="my"/> Remember me</label>
-                        </div>
-                    <div className="loginBox">
-                    <Button type={'submit'}>
-                            {t.common.login.loginButton}
-                        </Button>
-                    </div>
-                    </form>
                 </div>
-            </div>
-        </section>
-        </body>
+            </section>
+        </div>
     )
 }
 
