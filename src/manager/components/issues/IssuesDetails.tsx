96c27bd52fba1c1571f733ed5dc76c52fa8fa1b6
flavio <flaviozanda@gmail.com>
2021-06-22 20:17:23 +0200

    Add ant design

--- src/manager/components/issues/IssuesDetails.tsx
+++ src/manager/components/issues/IssuesDetails.tsx
@@ -1,13 +1,23 @@
 import { Typography } from "@material-ui/core"
+import { Button, Dropdown, Menu } from "antd"
 import { useEffect, useState } from "react"
 import { useHistory, useParams } from "react-router-dom"
-import { CommunityId, IssueId } from "../../../common/models/Types"
+import { CommunityId, Issue, IssueId } from "../../../common/models/Types"
 import { useTranslation } from "../../../common/Translator/UseTranslation"
 import { IssueResponse } from "../../models/responses/Responses"
 import { getIssue } from "../../services/IssuesService"
 
+const menu = (<Menu>
+        <Menu.Item>
+            <a>new</a>
+        </Menu.Item>
+        <Menu.Item>
+            <a>new</a>
+        </Menu.Item>
+    </Menu>)
+
 export const IssuesDetails = () => {
-    const [issue, setIssue] = useState<IssueId>()
+    const [issue, setIssue] = useState<Issue>()
     const history = useHistory()
     const { t } = useTranslation()
     const { issueId } = useParams<{ issueId: string }>()
@@ -15,7 +25,7 @@ export const IssuesDetails = () => {
     async function getIt() {
         return getIssue({ id: issueId })
             .then(iss => {
-                setIssue(issue);
+                setIssue(iss);
             })
     }
 
@@ -23,7 +33,22 @@ export const IssuesDetails = () => {
         () => { getIt() }
         , []);
 
+
     return <div className="page-appbar">
-        hgsdgbfvcbd
+        <div className="editBar">
+        <Dropdown overlay={menu}>
+                <Button onClick={(e) => e.preventDefault()}>
+        Status
+            </Button>
+        </Dropdown>
+        </div>
+        <div>
+            <Typography>
+                {issue?.subject}
+            </Typography>
+            <Typography>
+                {issue?.description}
+            </Typography>
+        </div>
     </div>
 }
